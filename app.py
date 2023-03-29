3cd55643ff59443ca4d96938938459d873bfc924
jrusso_relias <j.russo@relias.com>
2023-03-29 17:38:00 -0400

    Initial POC commit. Connects to OpenAI and allows user to ask questions, engaging the advanced features of the 'text-davinci-003' engine

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 import openai
 import json
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 secrets = None
+
+# clear the screen initially
+os.system('cls')  
 
 # get OpenAI API credentials
 try:
-    
-    openai.api_key = "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"
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     
     prompt = ''
     
-    while prompt != 'end': 
-        prompt = input("Ask OpenAI something (type 'end' to quit): ")
-    
-        if (prompt != 'end'):
-            response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=200)
+    while prompt != '2': 
+        print("***********************************")
+        print("**********OPENAI SEARCH************")
+        print("***********************************")
+        print("")
+        
+        prompt = input("Ask OpenAI something (1=cls, 2=end): ")
+        
+        if prompt == '1':
+            os.system('cls')    
+        elif (prompt != '2' and prompt != '1'):
+            response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, temperature=1, max_tokens=200)
             
-            print(f"response: {json.dumps(response.choices)}")
+            print(f"\nresponse: {response.choices[0].text}")
+            print("")
         
     print(f"OpenAI discussion ended.")
 except Exception as e:
-    print(f"An error occurred:{e.args[0]}")
+    print(f"An error occurred:{e}")
